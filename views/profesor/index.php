dcc123bf7a35b2349821ceb8a0555f9018a6ecd2
Alejandro Alfonzo <alex_alfonzo1@hotmail.com>
2017-05-21 11:14:11 -0500

    Profesores

--- views/profesor/index.php
+++ views/profesor/index.php
@@ -7,7 +7,7 @@ use kartik\grid\GridView;
 /* @var $searchModel app\models\SearchProfesor */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Profesors';
+$this->title = 'Profesores';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="profesor-index">
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Profesor', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear Profesor', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?=
     GridView::widget([
